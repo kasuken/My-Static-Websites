065544a6f887bdfda10b88c959cdc846ca7e3617
Emanuele Bartolesi <bartolesiemanuele@hotmail.com>
2021-09-09 17:43:55 +0200

    Update logo.js

--- emanuelebartolesi.com/src/components/logo.js
+++ emanuelebartolesi.com/src/components/logo.js
@@ -3,7 +3,8 @@ import { Link } from "gatsby"
 
 const Logo = props => (
   <div className="site-logo">
-    <Link to="/">{props.title}</Link>
+    {/* <Link to="/">{props.title}</Link> */}
+    <Link to="/">EB</Link>
   </div>
 )
 
